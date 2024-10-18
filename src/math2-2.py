8bb474cdbb78d896876aa6f3c8d7dd87db323f53
adprox <juwani0817@naver.com>
2024-10-18 17:26:43 +0900

    파일 업로드

---
+++ src/math2-2.py
@@ -0,0 +1,26 @@
+def partition(list,low,high):
+    i = low-1
+    pivot = list[high]
+    
+    for j in range(low, high):
+        if list[j] < pivot:
+            i = i+1
+            list[i], list[j] = list[j], list[i]
+    
+    list[i+1], list[high] = list[high], list[i+1]
+    return (i+1)
+
+def quickSort(list,low,high):
+    if low < high:
+        pi = partition(list,low,high)
+        quickSort(list, low, pi-1)
+        quickSort(list, pi+1, high)
+
+list = [10, 7, 3, 9, 1, 5]
+print("Original list =%s" % list)
+
+n = len(list)
+quickSort(list,0,n-1)
+
+print("Sorted list by Quicksort method")
+print(list)
